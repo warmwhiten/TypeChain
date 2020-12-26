99f8d866c971f30ca6e5ea355af1cd52893062a0
dahee kim <ekekekgml@naver.com>
2020-12-26 18:53:45 +0900

    add tsc-watch config

--- index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sayHi = (name, age, gender) => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`;
-};
-console.log(sayHi('warmwhiten', 22, 'female'));
-//# sourceMappingURL=index.js.map
