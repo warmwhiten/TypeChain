e7a9f188ddc908c6fd3e1060667b149c65c5081d
dahee kim <ekekekgml@naver.com>
2020-12-26 22:05:04 +0900

    #0.7 Classes on Typescript part One

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
-interface Human {
-	name:string;
-	age:number;
-	gender:string;
+class Human {
+	public name:string;
+	public age:number;
+	public gender:string;
+	constructor(name:string, age:number, gender:string){
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+	}
 }
 
-const person = {
-	name: 'warmwhiten',
-	age : 23,
-	gender: 'female'
-}
+const Dahee = new Human('warmwhiten', 22, 'female');
 
 const sayHi = (person:Human): string => {
 	return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`
 }
 
 
-console.log(sayHi(person));
+console.log(sayHi(Dahee));
 
 export {};
