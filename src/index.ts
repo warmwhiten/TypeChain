d97785331bd53494bc3a274f4caaee956f405389
dahee kim <ekekekgml@naver.com>
2020-12-26 21:34:31 +0900

    #0.6 Interfaces onTypescript

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
-const sayHi = (name:string, age:number, gender:string): string => {
-	return `Hello ${name}, you are ${age}, you are a ${gender}!`
+interface Human {
+	name:string;
+	age:number;
+	gender:string;
+}
+
+const person = {
+	name: 'warmwhiten',
+	age : 23,
+	gender: 'female'
+}
+
+const sayHi = (person:Human): string => {
+	return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`
 }
 
 
-console.log(sayHi('warmwhiten', 22,'female'));
+console.log(sayHi(person));
 
 export {};
